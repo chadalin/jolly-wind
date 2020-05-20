1e26e7f36190a6675c1589327e157ed3e5956546
dgimmibos <dgimmibos@yandex.ru>
2020-05-20 21:55:31 +0200

    rename

---
+++ public_html/index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<title></title>
+		<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+	</head>
+	<body>
+      <?php 
+		$zaiz = 1;
+		$volk = 12;
+		
+		
+		$result = $zaiz +  $volk;
+		
+        echo $result; ?>
+        <br>
+        <?php 
+        
+        ?>
+	</body>
+</html>
