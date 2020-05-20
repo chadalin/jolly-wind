d5e9e5e4bedcdbf8684e937204fcd61f61b01e99
dgimmibos <dgimmibos@yandex.ru>
2020-05-20 23:16:09 +0200

    edit file index.php

--- public_html/index.php
+++ public_html/index.php
@@ -1,21 +1,42 @@
-<!DOCTYPE html>
-<html>
-	<head>
-		<title></title>
-		<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
-	</head>
-	<body>
-      <?php 
-		$zaiz = 1;
-		$volk = 12;
-		
-		
-		$result = $zaiz +  $volk;
-		
-        echo $result; ?>
-        <br>
-        <?php 
-        
-        ?>
-	</body>
-</html>
+<?php
+if( !session_id() ) @session_start();
+require 'vendor/autoload.php';
+require 'app/components/rout.php';
+use DI\Container;
+use DI\ContainerBuilder;
+use League\Plates\Engine;
+use Tracy\Debugger;
+use App\controllers;
+
+Debugger::enable(Debugger::DEVELOPMENT);
+
+
+$dispatcher = rout();
+
+
+// Fetch method and URI from somewhere
+$httpMethod = $_SERVER['REQUEST_METHOD'];
+$uri = $_SERVER['REQUEST_URI'];
+
+// Strip query string (?foo=bar) and decode URI
+if (false !== $pos = strpos($uri, '?')) {
+    $uri = substr($uri, 0, $pos);
+}
+$uri = rawurldecode($uri);
+
+$routeInfo = $dispatcher->dispatch($httpMethod, $uri);
+switch ($routeInfo[0]) {
+    case FastRoute\Dispatcher::NOT_FOUND:
+        echo  '404 Not Found';
+        break;
+    case FastRoute\Dispatcher::METHOD_NOT_ALLOWED:
+        $allowedMethods = $routeInfo[1];
+        echo  '405 Method Not Allowed';
+        break;
+    case FastRoute\Dispatcher::FOUND:
+        $handler = $routeInfo[1];
+        $vars = $routeInfo[2];
+        $container = new Container();
+        $container->call($handler, $vars);
+        break;
+}
